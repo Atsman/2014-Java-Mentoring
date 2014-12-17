ac546babdbda3339a54583c6dacd915f6948c764
alehatsman <olegruno@gmail.com>
2014-12-17 11:17:45 +0300

    Unit test. spring annotation

---
+++ spring-annotations/src/test/java/com/epam/mentoring/spring/MessageServiceTest.java
@@ -0,0 +1,30 @@
+package com.epam.mentoring.spring;
+
+import com.epam.mentoring.spring.configuration.AppConfig;
+import com.epam.mentoring.spring.service.MessageService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by alehatsman on 12/17/14.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppConfig.class})
+public class MessageServiceTest {
+
+    @Autowired
+    private MessageService messageService;
+
+    @Test
+    public void test() {
+        Assert.assertEquals("Default", messageService.getMessage());
+        messageService.setMessage("My message");
+        Assert.assertEquals("My message", messageService.getMessage());
+    }
+
+}
